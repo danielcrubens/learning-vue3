63e3ede0f421a63ce9bd16438cc4adc87ea8dd0f
danielcrubens <dcr.tecnologi@gmail.com>
2022-12-10 17:50:16 -0300

    lifecycle e  hooks  :white_check_mark:

---
+++ 6_lifecycle_hooks/src/components/LifeCycle.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Meu nome é: {{nome}}</h1>
+  </div>
+</template>
+
+<script>
+export default{
+  name:'LifeCycle',
+  data() {
+    return {
+      nome:'Ainda não sei'
+    }
+  },
+  created(){
+    setTimeout(()=>{
+      this.nome ='Daniel'
+    },1000)
+  },
+  mounted(){
+    setTimeout(()=>{
+      this.nome ='Felipe'
+    },2000)  }
+}
+</script>
