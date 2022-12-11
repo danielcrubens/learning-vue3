9c7940a12af20165281059f6f3c1024b486c4e36
danielcrubens <dcr.tecnologi@gmail.com>
2022-12-11 12:43:34 -0300

    hierarquia de componentes  :white_check_mark:

---
+++ 7_hierarquia_componentes/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+  <LifeCycle />
+  <Pessoa/>
+</div>
+</template>
+
+<script>
+import LifeCycle from './components/LifeCycle.vue';
+import Pessoa from'./components/Pessoa.vue'
+
+export default {
+  name: 'App',
+  components: {
+    LifeCycle,
+    Pessoa
+  },
+  data() {
+    return {
+    }
+  },
+}
+</script>
