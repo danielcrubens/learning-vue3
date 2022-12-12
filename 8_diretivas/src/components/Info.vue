b5b270dda0011e65786bf56383c519cfcd000736
danielcrubens <dcr.tecnologi@gmail.com>
2022-12-12 20:39:50 -0300

    diretivas  :white_check_mark:

---
+++ 8_diretivas/src/components/Info.vue
@@ -0,0 +1,17 @@
+<template>
+  <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
+  <p v-else>Estou em busca de novas oportunidades!</p>
+
+  <p v-show="mostrar_email">Mande uma mensagem para: dcr.tecnologi@gmail.com</p>
+</template>
+<script>
+export default {
+  name: 'Info',
+  data() {
+    return { 
+      esta_trabalhando: false,
+      mostrar_email: false
+    }
+  }
+}
+</script>
