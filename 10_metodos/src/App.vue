97ef31c7448dfa2032e307a015d9bf2a2803c33a
danielcrubens <dcr.tecnologi@gmail.com>
2022-12-13 20:31:35 -0300

    métodos  :white_check_mark:

---
+++ 10_metodos/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <LifeCycle/>
+    <Info/>
+  </div>
+</template>
+
+<script>
+import LifeCycle from './components/LifeCycle.vue';
+import Info from './components/Info';
+
+
+export default {
+  name: 'App',
+  components: {
+    LifeCycle,
+    Info
+  },
+  data() {
+    return {
+    }
+  },
+}
+</script>
---
+++ 10_metodos/src/components/Info.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+  <button @click="showEmail">{{textoBotao}}</button>
+  <p v-show="mostrar_email">Mande uma mensagem para:{{email}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Info',
+
+  data() {
+    return { 
+      esta_trabalhando: false,
+      mostrar_email: false,
+      meu_link:'https://danielcrubens.github.io',
+      email:'dcr.tecnlogi@gmail.com',
+      textoBotao:'Mostrar e-mail'
+    }
+  },
+  methods:{
+    showEmail(){
+      console.log("entrou")
+      this.mostrar_email = !this.mostrar_email
+      if (!this.mostrar_email) {
+        this.textoBotao ='Mostrar e-mail'
+      }else{
+        this.textoBotao ='Esconder e-mail' 
+      }
+    }
+  }
+}
+</script>
