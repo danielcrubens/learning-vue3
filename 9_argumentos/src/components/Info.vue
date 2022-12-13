759d9d8cd703514eba766978a52812635869994a
danielcrubens <dcr.tecnologi@gmail.com>
2022-12-12 21:12:19 -0300

    argumentos  :white_check_mark:

---
+++ 9_argumentos/src/components/Info.vue
@@ -0,0 +1,21 @@
+<template>
+  <p>Para acessar me portfólio <a v-bind:href="meu_link" target="_blank">basta clicar aqui</a></p>
+  <Picture />
+</template>
+
+<script>
+import Picture from './Picture.vue'
+export default {
+  name: 'Info',
+  components:{
+    Picture
+  },
+  data() {
+    return { 
+      esta_trabalhando: false,
+      mostrar_email: false,
+      meu_link:'https://danielcrubens.github.io'
+    }
+  }
+}
+</script>
