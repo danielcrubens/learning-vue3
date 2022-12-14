3761531896061bbf71ad0c2d03efe3fd2cc656fa
danielcrubens <dcr.tecnologi@gmail.com>
2022-12-14 14:54:44 -0300

    CSS scoped e CSS global  :white_check_mark:

---
+++ 11_escoped_css/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <Header/>
+  <div>
+    <LifeCycle/>
+    <Info/>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import LifeCycle from './components/LifeCycle.vue';
+import Info from './components/Info';
+
+
+export default {
+  name: 'App',
+  components: {
+    LifeCycle,
+    Info,
+    Header
+  },
+  data() {
+    return {
+    }
+  },
+}
+</script>
+<style>
+body{
+  background-color: #333;
+  color:#fff;
+}
+</style>
---
+++ 11_escoped_css/src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">Projeto</a></li>
+      <li><a href="#">Contato</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Header'
+}
+</script>
+<style scoped>
+ul{
+  list-style-type: none;
+  display: flex;
+  align-items: center;
+  justify-content: center ;
+}
+li{
+  margin-right: 10px;
+}
+a{
+  color: #DDD;
+  text-decoration: none;
+  transition: .5s;
+  font-weight: bold;
+}
+a:hover{
+  color: turquoise;
+}
+
+</style>
