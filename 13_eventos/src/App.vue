b653d89b8d7343bf3dfc8b964e66ac24febd46ae
danielcrubens <dcr.tecnologi@gmail.com>
2022-12-16 14:15:06 -0300

    eventos  :white_check_mark:

---
+++ 13_eventos/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Form/>
+  </div>
+</template>
+<script>
+
+import Form from './components/Form.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Form
+  },
+  
+}
+</script>
---
+++ 13_eventos/src/components/Form.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h1> Meu Formulário</h1>
+    <form action="" @submit.prevent="enviarFormulario">
+
+      <div>
+        <input type="text" v-model="name">
+      </div>
+      <div>
+        <input type="text" v-model="email">
+      </div>
+      <div>
+        <Submit />
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import InputText from './form/InputText.vue'
+import Submit from './form/Submit.vue'
+export default {
+  name: 'Form',
+  components: {
+    InputText,
+    Submit
+  },
+  data() {
+    return {
+      name:'',
+      email:''
+    }
+  },
+  methods: {
+    enviarFormulario(){
+     
+      const name = this.name;
+      const email = this.email
+      console.log('formulário enviado!')
+      console.log(`O nome é: ${name}`)
+      console.log(`O e-mail é ${email}`)
+      
+    }
+  },
+}
+</script>
