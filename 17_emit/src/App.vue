c7548daf0928a9043d717b9e2427ca1d40e7a538
danielcrubens <dcr.tecnologi@gmail.com>
2022-12-17 16:05:25 -0300

    emit  :white_check_mark:

---
+++ 17_emit/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Picture />
+  </div>
+</template>
+<script>
+
+import Picture from './components/Picture.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Picture
+  },
+  
+}
+</script>
---
+++ 17_emit/src/components/Picture.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <img :src="avatar" :alt="descricao">
+    <MudarImagem @mudar-imagem="trocarImagem" />
+  </div>
+</template>
+
+<script>
+import MudarImagem from './MudarImagem.vue'
+export default {
+  name: 'Picture',
+  components: {
+    MudarImagem
+  },
+  data() {
+    return {
+      avatar: '/img/avatar.png',
+      descricao: 'Daniel Rubens'
+    }
+  },
+  methods: {
+    trocarImagem() {
+      this.avatar = '/img/avatar2.png'
+      console.log('FOI')
+    }
+  },
+}
+</script>
